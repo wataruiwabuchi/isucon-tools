d384253027df511b7701dfb242893188098422b1
wataruiwabuchi <speeeedcubist@gmail.com>
2020-07-24 11:51:27 +0900

    Create make_tmux_session.sh

---
+++ make_tmux_session.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ue
+
+SESSION_NAME="isucon"
+SENDED_COMMANDS=(
+  #"ssh image1"
+  "cd /home/isucon/isubata/webapp/go"
+)
+WINDOW_NAMES=("bench" "app" "alp" "slow-query" "wlreporter")
+
+tmux new-session -d  -s $SESSION_NAME
+
+for WINDOW_NAME in "${WINDOW_NAMES[@]}"
+do
+  tmux new-window -n $WINDOW_NAME
+
+  for COMMAND in "${SENDED_COMMANDS[@]}"
+  do  
+    tmux send-keys "$COMMAND" C-m
+  done
+done
+
+tmux attach -t $SESSION_NAME
