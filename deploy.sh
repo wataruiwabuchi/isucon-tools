2b8e0e42d3207a4ece8212a566f21afad2a488cc
wataruiwabuchi <speeeedcubist@gmail.com>
2021-08-02 23:43:17 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,55 @@
 #!/usr/bin/env bash
-set -ex
-export PATH=$HOME/local/go/bin:$PATH
+set -eux
 
-GITHOME=`cd $(dirname "${0}") && cd .. && pwd`
+# 3つのモードを実装する
+# 0: current git status & current server
+# 1: arg branch & current server
+# 2: arg branch & multi server
+
+GIT_TOP_PATH=$( git rev-parse --show-toplevel )
+
+SERVERS=(
+    $( hostname )
+)
 
 LOG_FILES=(
     /var/log/mysql/mysql-slow.log
+    /var/log/mysql/access.log
     /var/log/mysql/error.log
 )
 
 SERVICES=(
     nginx
     mysql
-    isubata.golang
+    isucari.python
 )
 
-DATE=`date "+%Y%m%d_%H%M%S"`
+DATE=$( date --iso-8601=seconds )
 
-# Move to working directory
-cd "${GITHOME}"
+source ./deployed_file_paths.sh
 
-# Update git
-# git checkout master
-# git stash
-# git pull origin master
-# git stash apply stash@{0}
+for server in ${SERVERS[@]}
+do
+    # Update git                                                                                                                                                                                                                                 # git checkout master
+    # git stash
+    # git pull origin master
+    # git stash apply stash@{0}
 
-# Rotate log files
-for LOG_FILE in "${LOG_FILES[@]}"; do
-    sudo test -f "${LOG_FILE}" && sudo mv "${LOG_FILE}" "${LOG_FILE}.${DATE}"
+    # Rotate log files
+    for LOG_FILE in "${LOG_FILES[@]}"; do
+        ssh isucon@${server} "sudo test -f ${LOG_FILE} && sudo mv ${LOG_FILE} ${LOG_FILE}.${DATE} || echo 1"
+    done
+
+    # locate file
+    for common_path in ${COMMON_PATHS[@]}
+    do
+        rsync -auvz -e ssh --rsync-path='sudo rsync' $( basename ${common_path} ) isucon@${server}:${common_path}
+    done
+    for uncommon_path in ${UNCOMMON_PATHS[@]}
+    do
+        rsync -auvz -e ssh --rsync-path='sudo rsync' $( basename ${uncommon_path} ).${server} isucon@${server}:${uncommon_path}
+    done
+
+    # Restart services
+    ssh isucon@${server} sudo systemctl restart "${SERVICES[@]}"
 done
-
-cd "${HOME}/isubata/webapp/go"
-make
-
-# Restart services
-sudo systemctl restart "${SERVICES[@]}"
