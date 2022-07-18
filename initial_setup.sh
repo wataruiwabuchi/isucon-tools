5adb2a63eee740132f5c2e03e0db8e29fc042e99
wataruiwabuchi <speeeedcubist@gmail.com>
2022-07-18 13:22:10 +0900

    バックアップ部分が必要なさそうなのでコメントアウト

--- initial_setup.sh
+++ initial_setup.sh
@@ -23,9 +23,9 @@ BACKUP_TARGETS=(
 source ./deployed_file_paths.sh
 
 # 初期バックアップの取得
-sudo mkdir -p ${BACKUP_DIR} && sudo chown -R isucon:isucon ${BACKUP_DIR}
-sudo mysqldump -x --all-databases > ${MYSQL_DUMP_PATH}
-sudo tar cvzf ${BACKUP_DIR}/backup.tar.gz ${BACKUP_TARGETS[@]} && sudo chown isucon:isucon ${BACKUP_DIR}/backup.tar.gz
+# sudo mkdir -p ${BACKUP_DIR} && sudo chown -R isucon:isucon ${BACKUP_DIR}
+# sudo mysqldump -x --all-databases > ${MYSQL_DUMP_PATH}
+# sudo tar cvzf ${BACKUP_DIR}/backup.tar.gz ${BACKUP_TARGETS[@]} && sudo chown isucon:isucon ${BACKUP_DIR}/backup.tar.gz
 
 # githubから取得した公開鍵をauthorized_keysに配置
 # TODO 他サーバの考慮をどうするか
